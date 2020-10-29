658047c0024f6e9402befc364a51dc8038181177
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-29 22:27:35 +0530

    Added: Gambler reached at 50%

--- Glambler.sh
+++ Glambler.sh
@@ -2,6 +2,7 @@
 echo "Welcome in Gambler Simulation Problem"
 Stake=100
 Bet=1
+Goal=50
 winloss=$((1+RANDOM%2))
 if [ $winloss -eq 1 ]
 then
@@ -9,4 +10,10 @@ then
 else
 	echo "Gambler Loss"
 fi
-
+if [ $Stake -gt 50 ]
+then
+	echo "Gambler won &  he continue"
+else
+	echo "Gambler loss the game drop the game"
+fi
+	
