38a742bca2fe537cdb078fc99b2983d51c4d2c9d
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-29 22:15:30 +0530

    Added: Gambler is Won or loss

--- Glambler.sh
+++ Glambler.sh
@@ -1,6 +1,12 @@
-                     Glambler.sh
 #!/bin/bash -x
-echo "Welcome in Glambler Simulation Problem"
-stake=100
+echo "Welcome in Gambler Simulation Problem"
+Stake=100
 Bet=1
+winloss=$((1+RANDOM%2))
+if [ $winloss -eq 1 ]
+then
+	echo "Gambler Won"
+else
+	echo "Gambler Loss"
+fi
 
